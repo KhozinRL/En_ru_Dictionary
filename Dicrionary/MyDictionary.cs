052b76324a4bf6b54956c33812b39e49172df2ad
Р.Л. Хозин <s190291019@live.cs.msu.ru>
2021-02-11 11:55:49 +0300

    Add project files.

---
+++ Dicrionary/MyDictionary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MyDictionary
+{
+    private Dictionary<string, HashSet<string>> dictionaryList = new Dictionary<string, HashSet<string>>();
+
+
+	public MyDictionary()
+	{
+        Add("car", new HashSet<string> { "машина" });
+        Add("verbose", new HashSet<string> { "подробный", "многословный" });
+        Add("waiter", new HashSet<string> { "официант", "поднос", "подавальщик" });
+        Add("радуга", new HashSet<string> { "rainbow" });
+    }
+
+    public void Add(string word, HashSet<string> translations)
+    {
+        word = word.ToLower().Trim();
+        if (word == "")
+            return;
+
+        string s;
+        HashSet<string> lowerHashSet = new HashSet<string>();
+
+        foreach (string str in translations) { 
+            s = str.ToLower().Trim();
+            if (s == "") continue;
+
+            lowerHashSet.Add(s);
+            addTranslation(s, word);
+        }
+
+        if (lowerHashSet.Count != 0)
+            addTranslations(word, lowerHashSet);
+    }
+
+    //Добавляет набор переводов
+    private void addTranslations(string word, HashSet<string> translations) {
+        if (dictionaryList.ContainsKey(word))
+        {
+            dictionaryList[word].UnionWith(translations);
+        }
+        else {
+            dictionaryList.Add(word, translations);
+        }
+    }
+
+    //Добавляет единственный перевод
+    private void addTranslation(string word, string translation) {
+        if (dictionaryList.ContainsKey(word))
+        {
+            dictionaryList[word].Add(translation);
+        }
+        else {
+            dictionaryList.Add(word, new HashSet<string> { translation });
+        }
+    }
+
+    public HashSet<string> GetTranslation(string word) {
+        HashSet<string> result;
+        return dictionaryList.TryGetValue(word, out result) ? result : new HashSet<string>();
+    }
+
+    override
+    public string ToString() {
+        StringBuilder sb = new StringBuilder();
+        foreach (KeyValuePair<string, HashSet<string>> kvp in dictionaryList){
+            sb.Append(kvp.Key).Append(" : ");
+            foreach (string str in kvp.Value) {
+                sb.Append(str).Append(", ");
+            }
+            sb.Remove(sb.Length - 2, 2).Append('\n'); ;
+        }
+        return sb.ToString();
+    }
+}
---
+++ Dicrionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace MyDicrionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary myDictionary = new MyDictionary();
+            myDictionary.Add("машина", new HashSet<string>() { " car " });
+            myDictionary.Add(" rainbow ", new HashSet<string>() { " радуга " });
+            Console.WriteLine(myDictionary.ToString());
+
+            string word, translation;
+            HashSet<string> translationSet;
+
+            while(true)
+            {
+                Console.Write("Введите слово для перевода: ");
+                word = Console.ReadLine();
+                if (word == "") break;
+                Console.Write("Введите перевод (один или несколько через запятую): ");
+                translation = Console.ReadLine();
+                translationSet = new HashSet<string>(translation.Split(','));
+                myDictionary.Add(word, translationSet);
+            }
+
+            /*word = Console.ReadLine();
+            translation = Console.ReadLine();
+            translationSet = new HashSet<string>(translation.Split(','));
+            myDictionary.Add(word, translationSet);*/
+
+            Console.WriteLine(myDictionary.ToString());
+        }
+    }
+}
